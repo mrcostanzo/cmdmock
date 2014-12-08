9de5bc7f99810d3de675a39c0d8349275c9c1007
M. Costanzo <mcostanzo@bnl.gov>
2014-12-07 21:13:28 -0500

    InvocationMap class fleshed out some more, can now build vocabulary. Added some temp output files for reference

--- cmdmock.py
+++ cmdmock.py
@@ -4,11 +4,13 @@
 
 """ cmdmock is a simple utility to create a mock version of a command with a specific
 argument string (or no arguments). It runs the command, takes the output and hard-codes that
-output as a canned response which can then be called and regurigtated. It then tries to set
+output as a canned response which can then be called and regurgitated. It then tries to set
 the file to be executable so a module under test or development can call it transparently
 """
 
-#TODO add hash logic (output hash primary, input hash secondary)
+__version__ = '0.04'
+
+#TODO add time to auto-generated module docstring
 #TODO consider supporting options before the command to change behavior
 #TODO optionally enforce exact argument string or return an error
 #TODO optionally build up the vocabulary of the mocker by running multiple arguments
@@ -31,11 +33,9 @@ def handle_args():
     """Parse out arguments"""
     parser = optparse.OptionParser()
     parser.add_option('-i', '--interactive', action='store_true', \
-                      help='Interactively enter a series of invocations'
-                      )
+                      help='Interactively enter a series of invocations')
     parser.add_option('-f', '--file', dest='training_file', \
-                      help='Optionally specify a training file with line-seperated invocations'
-                      )
+                      help='Optionally specify a training file with line-seperated invocations')
     #parser.add_option('-v', '--verbose', action='store_true', dest='verbose', default=False,
     #                  help='Print informative messages')
     #parser.add_option('-d', '--debug', action='store_true', dest='debug', default=False,
@@ -44,49 +44,66 @@ def handle_args():
     return (options, args)
 
 
-class invocation_set:
-    """ Container to hold, add, and serialize invocations and responses for building a vocalbulary.
-    Inputs are stored as a list of arguments, the same way argv does it, they are also hashed using 
-    md5 for searching and indexing. Command responses are stored as big strings along with their 
+class InvocationSet(object):
+    """ Container to hold, add, and serialize invocations and responses for building a vocabulary.
+    Inputs are stored as a list of arguments, the same way argv does it, they are also hashed using
+    md5 for searching and indexing. Command responses are stored as big strings along with their
     hashes for easier indexing and comparison. """
-    
-    def __init__(self):
-        """ Create empty invocation set """
+
+    def __init__(self, cmd):
+        """ Create empty invocation set for command cmd. We will enforce that all invocations
+        begin with cmd or the program will exit with an error. In file mode cmd is the command
+        of the first invocation, in interactive mode it is the first command passed """
+        self.cmd = cmd          #root command, present in all invocations
         self.invocations = {}   #collection of invocation arg permutations, indexed by their hashes
         self.responses = {}     #collection of output responses, indexed by their hashes
         self.call_map = {}      #mapping of what invocations yield what outputs (1-to-1 or many-to-1)
-        
+
     def add_invocation(self, invocation):
         """ Add invocation to the set, including output and mapping.
         invocation should be a list in the same format as argv provides including the command itself
          """
-        log.debug("Invoking %s", str(invocation))
+        if invocation[0] != self.cmd:
+            log.error("Invocation %s does not match command '%s'", invocation, self.cmd)
+            raise ValueError
+
+        #log.debug("Invoking %s", str(invocation))
         invocation_hash = hashlib.md5(str(invocation)).hexdigest()
-        log.debug("Invocation hash is %s", invocation_hash)
+        #log.debug("Invocation hash is %s", invocation_hash)
         output = get_response(invocation)
         output_hash = hashlib.md5(output).hexdigest()
-        log.debug("Output hash was %s", output_hash)
-        
+        #log.debug("Output hash was %s", output_hash)
+
+        if (output_hash not in self.responses) and (invocation_hash in self.invocations):
+            log.warn("New response for equavalent invocation: %s. Does command print time?", \
+                     invocation)
+
         if output_hash not in self.responses:       #if output is new add it
             self.responses[output_hash] = output
-        
-        if invocation_hash not in self.invocations:
+            log.debug("Invocation %s: adding new output (hash = %s)", invocation, output_hash)
+
+        if invocation_hash not in self.invocations:         #if input is new add to invocations
             self.invocations[invocation_hash] = invocation
-        
-        if invocation_hash not in self.call_map:
-            self.call_map[invocation_hash] = output_hash
-        
-        #log.debug("Output was:\n%s", output) 
-    
+            log.debug("Invocation %s: adding new input hash: %s", invocation, invocation_hash)
+
+        self.call_map[invocation_hash] = output_hash    # unconditionally add or update the map
+        log.debug("Invocation %s: setting map: (%s : %s)\n", invocation, invocation_hash, \
+                  self.call_map[invocation_hash])
+
+        #log.debug("Output was:\n%s", output)
+
     def summarize(self):
         """ Get stats and printouts for debugging """
+        log.debug("\n%d invocations mapped to %d outputs via %d map entries\n\n", \
+                  len(self.invocations), len(self.responses), len(self.call_map))
         log.debug("Invocations:\n%s", str(self.invocations))
         log.debug("Mapping:\n%s", str(self.call_map))
-        log.debug("Outputs:\n%s", str(self.responses))
-    
-    def serialze(self):
+        #log.debug("Outputs:\n%s", str(self.responses))
+
+    def serialize(self):
         """ Generate the code text for all invocations to be written into the output file """
-        return str(dir(self))    #one step better than pass
+        vocab_string = "CALL_MAP = %s\n\nOUTPUTS = %s" %(str(self.call_map), str(self.responses))
+        return vocab_string
 
 
 def get_response(arg_list):
@@ -107,11 +124,11 @@ def write_mock_cmd(cmd, output):
     full_invocation = str(sys.argv[1:])
     argument_list = str(sys.argv[2:])
 
-    doc_string = '"""This module was generated by mockcmd.py ' + \
-    'on ' + date + ' called by ' + caller + '\nwith the following invocation:\n' + \
+    doc_string = '"""This module was generated by mockcmd.py version ' + __version__ + \
+    '\non ' + date + ' called by ' + caller + '\nwith the following invocation:\n' + \
     full_invocation + '\n"""\n'
-    
-    import_string = "import sys\n\n"
+
+    import_string = "\nimport sys\n\n"
 
     canned_string = "CANNED_OUTPUT  = '''" + output + "'''\n\n"
 
@@ -121,7 +138,7 @@ def write_mock_cmd(cmd, output):
                   '\telse:\n' \
                   '\t\tprint "Unsupported argument"\n' \
                   '\t\traise ValueError\n\n'
-    
+
     exec_string = '''if __name__ == "__main__":\n\tsys.exit(main(sys.argv))'''
 
     try:
@@ -144,24 +161,36 @@ def write_mock_cmd(cmd, output):
     subprocess.Popen(chmod_args, stdout=subprocess.PIPE)
 
 
+def write_serialization(vocab):
+    """ Quick test of serialization """
+    with open('test_serial.py', 'w', 0) as fp:
+        fp.write("#Simple test of vocabulary serialization routine\n\n")
+        fp.write(vocab.serialize())
+
+
 def main(argv):
     """ Main module. """
     #(options, args) = handle_args()
 
     log.basicConfig(format="[%(levelname)s]: %(message)s", level=log.DEBUG)
-    
-    vocab = invocation_set()    #initiate the empty vocabulary
-    vocab.add_invocation(argv[1:])
-    vocab.summarize()
-    #print vocab.serialze() 
 
-"""
+    vocab = InvocationSet(argv[1])    #initiate the empty vocabulary for command
+    vocab.add_invocation(argv[1:])
+    vocab.add_invocation(['ls', '-al'])
+    vocab.add_invocation(['ls', '-a', '-l'])
+    vocab.add_invocation(['ls', '-alh'])
+    #vocab.add_invocation(['echo', '"Some text"'])  #test to bomb
+    vocab.summarize()
+    write_serialization(vocab)
+    #print vocab.serialze()
+
+
     passed_options = argv[1:]
     log.info("Options passed were: %s", passed_options)
     response = get_response(passed_options)
     log.info("Response was:\n%s", response)
     write_mock_cmd(argv[1], response)
-"""
+
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
